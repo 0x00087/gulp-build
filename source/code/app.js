69678410f0af3104dfb015d70ffbec7fd3713b4d
0x00087 <ewaulence@gmail.com>
2022-06-21 17:23:52 +0300

    dev

--- source/code/app.js
+++ source/code/app.js
@@ -1,3 +1,29 @@
-import * as flsFunction from './modules/function.js'
+import * as flsFunction from './modules/function.js';
 
-flsFunction.isWebp()
+flsFunction.isWebp();
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  allowTouchMove: false,
+
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+
+  breakpoints: {
+    1280: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    708: {
+      slidesPerView: 2,
+    },
+  },
+});
